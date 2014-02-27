8ee5dae5f9bf7246d4e4155239b404cb95ff84ab
Andreas Lutro <anlutro@gmail.com>
2014-02-27 12:01:00 +0100

    add test for errors

---
+++ tests/unit/ValidatedRepositoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Mockery as m;
+
+class ValidatedRepositoryTest extends PHPUnit_Framework_TestCase
+{
+	public function testErrorsAreAddedAndCanBeRetrieved()
+	{
+		$model = m::mock('Illuminate\Database\Eloquent\Model');
+		$model->shouldReceive('getTable')->once()->andReturn('table');
+		$validator = m::mock('c\Validator');
+		$validator->shouldReceive('replace')->once()->with('table', 'table');
+		$repo = new ValidatedRepositoryStub($model, $validator);
+
+		$model->shouldReceive('newInstance->fill->save')->once()->andReturn(true);
+		$validator->shouldReceive('validCreate')->andReturn(false);
+		$validator->shouldReceive('errors')->andReturn(new Illuminate\Support\MessageBag(['error' => ['message']]));
+		$repo->create(['foo' => 'bar']);
+		$errors = $repo->getErrors();
+		$this->assertInstanceOf('Illuminate\Support\MessageBag', $errors);
+		$this->assertEquals(['error' => ['message']], $errors->getMessages());
+	}
+}
+
+class ValidatedRepositoryStub extends \c\EloquentRepository {}
