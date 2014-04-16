d415066d8ed294d6eacd5bb947b661311c4ef820
Andreas Lutro <anlutro@gmail.com>
2014-04-16 12:09:40 +0200

    update example [ci skip]

--- examples/ProjectRepository.php
+++ examples/ProjectRepository.php
@@ -48,6 +48,56 @@ class ProjectRepository extends \anlutro\LaravelRepository\EloquentRepository
     }
 
     /**
+     * You can add as many setter/getter methods to your repositories as you
+     * wish if you need extra behaviour. For this example, let's assume the
+     * repository needs access to the currently logged in user. Instead of using
+     * Auth::user() in the repository, we add a setter method which we can call
+     * from the controller - $repo->setCurrentUser(Auth::user());
+     */
+    public function setCurrentUser(MyUserModel $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Determines if a model can be stored to the database or not. Use it for
+     * stuff that can't (easily) be done by the validator.
+     *
+     * Changed from readyForCreate in 0.5
+     */
+    protected function beforeCreate($project, $attributes)
+    {
+        // each project has an owner (a user). let's make sure that the project
+        // owner is an active user - if not, add an error and return false. we
+        // assume that project->owner is always populated.
+        if (!$project->owner->is_active) {
+            $this->errors->add('owner', 'Project owner must be an active user.');
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Using afterSave, afterCreate or afterUpdate to set relationships is
+     * often very useful. afterSave is called on both update and create.
+     */
+    public function afterSave($project, $attributes)
+    {
+        // if "relation" is not present in the input, we simply detach all
+        // related models by passing an empty array.
+        $ids = isset($attributes['relation']) ? $attributes['relation'] : [];
+        $project->manyToManyRelation()->sync($ids);
+    }
+
+    public function afterCreate($project, $attributes)
+    {
+        if (!$project->user && $this->user) {
+            $project->owner()->associate($this->user);
+        }
+    }
+
+    /**
      * This method is called before fetchMany and fetchSingle. We use it to add
      * functionality that should be present on every query.
      *
@@ -64,60 +114,59 @@ class ProjectRepository extends \anlutro\LaravelRepository\EloquentRepository
         $query->with('members', 'comments');
 
         // let's say guests can only view projects with access level 1 or less.
-        if (!Auth::check()) {
+        if (!$this->user) {
             $query->where('access_level', '<=', 1);
-            return;
         }
-
-        $user = Auth::user();
 
         // unless user is an admin, we restrict every query to only show
         // projects with access_level lower than the logged in user's.
-        if (!$user->isAdmin()) {
+        else if (!$this->user->isAdmin()) {
             $query->where('access_level', '<=', $user->access_level);
         }
     }
 
     /**
-     * This method is called after fetchMany when not paginating. We use it to
-     * perform operations on a collection of models before it is returned
-     * from the repository.
+     * This method is called after fetchMany and fetchSingle. It is most useful
+     * to append attributes onto more than one model for stuff that can't easily
+     * be done via the query builder. Sometimes, for example, a subselect or
+     * join or groupby can mess with the paginator.
      *
      * Changed from prepareCollection/preparePaginator in 0.5
      */
-    protected function afterQuery($projects)
+    protected function afterQuery($result)
     {
+        // $result can be a single model, a paginator or a collection. We can
+        // standardize the variable in this way to be able to use the same code
+        // on all three types.
+        if ($result instanceof \Illuminate\Database\Eloquent\Model) {
+            // we might just do `return;` here if we didn't want our logic
+            // applied to a single model.
+            $projects = $result->newCollection([$result]);
+        } else if ($result instanceof \Illuminate\Pagination\Paginator) {
+            $projects = $result->getCollection();
+        }
+
         // let's say we want to add some data onto the models after they've
-        // been retrieved from the database. we want "ExtraData" which is a
-        // month or less old.
-        $dt = Carbon\Carbon::now()->subMonth();
-        $extraData = ExtraData::where('created_at', '>', $dt)->get();
+        // been retrieved from the database. we want to add the count of a
+        // relation, but don't want to mess with sub-selects and don't want to
+        // waste memory by eager loading the relation, nor do we want to run
+        // one query for each model.
 
-        // iterate through the collection of projects...
-        foreach ($projects as $project) {
-            // filter out the extra data related to the project using our own logic
-            $project->extra_data = $extraData->filter(function($extra) use($project) {
-                return $extra->is_project && $extra->user_id == $project->owner_id;
-            });
-        }
-    }
+        // build the query that gets the relationship count. some of this logic
+        // could be moved into model scopes if you want. this is a rather
+        // complex query so if you don't understand what is going on here,
+        // don't worry too much. basically just getting the number of relations
+        // attached to each project.
+        $results = $this->model->relation()
+            ->getRelated()->newQuery()
+            ->whereIn('parent_id', $projects->lists('id'))
+            ->selectRaw('parent_id, count(*) as relation_count')
+            ->groupBy('parent_id')
+            ->lists('relation_count', 'parent_id');
 
-    /**
-     * Determines if a model can be stored to the database or not. Use it for
-     * stuff that can't (easily) be done by the validator.
-     *
-     * Changed from readyForCreate in 0.5
-     */
-    protected function beforeCreate($project)
-    {
-        // each project has an owner (a user). let's make sure that the project
-        // owner is an active user - if not, add an error and return false. we
-        // assume that project->owner is always populated.
-        if (!$project->owner->is_active) {
-            $this->errors->add('owner', 'Project owner must be an active user.');
-            return false;
-        }
-
-        return true;
+        // now add the attribute to each model.
+        $projects->each(function($project) {
+            $project->relation_count = $results[$project->id];
+        });
     }
 }
