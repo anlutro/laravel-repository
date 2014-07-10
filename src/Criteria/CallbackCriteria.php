a6335445df81a4038d148f331f9814d4077f7c1f
Andreas Lutro <anlutro@gmail.com>
2014-07-10 11:22:25 +0200

    add callback criteria

---
+++ src/Criteria/CallbackCriteria.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Laravel 4 Repository classes
+ *
+ * @author   Andreas Lutro <anlutro@gmail.com>
+ * @license  http://opensource.org/licenses/MIT
+ * @package  l4-repository
+ */
+
+namespace anlutro\LaravelRepository\Criteria;
+
+use anlutro\LaravelRepository\CriteriaInterface;
+
+/**
+ * Class that stores and applies a callback as a criteria.
+ */
+class CallbackCriteria implements CriteriaInterface
+{
+	protected $callback;
+
+	public function __construct(callable $callback)
+	{
+		$this->callback = $callback;
+	}
+
+	public function apply($query)
+	{
+		call_user_func($this->callback, $query);
+	}
+}
