b8f79f8fb3f26d743666eebe38c050a5a673fc0a
Andreas Lutro <anlutro@gmail.com>
2015-01-28 08:16:58 +0100

    make search criteria behaviour configurable - closes #14

--- src/Criteria/SearchCriteria.php
+++ src/Criteria/SearchCriteria.php
@@ -13,6 +13,9 @@ use anlutro\LaravelRepository\CriteriaInterface;
 
 class SearchCriteria implements CriteriaInterface
 {
+	const LOGIC_AND = 1;
+	const LOGIC_OR = 2;
+
 	/**
 	 * @var array
 	 */
@@ -24,13 +27,27 @@ class SearchCriteria implements CriteriaInterface
 	protected $search;
 
 	/**
+	 * Whether to use AND or OR.
+	 *
+	 * @var int
+	 */
+	protected $logic;
+
+	/**
 	 * @param array  $searchableColumns
 	 * @param string $searchFor
+	 * @param int    $logic
 	 */
-	public function __construct(array $searchableColumns, $searchFor)
+	public function __construct(array $searchableColumns, $searchFor, $logic = self::LOGIC_AND)
 	{
 		$this->columns = $searchableColumns;
 		$this->search = $searchFor;
+
+		if ($logic !== static::LOGIC_AND && $logic !== static::LOGIC_OR) {
+			throw new \InvalidArgumentException('Invalid value for $logic, use one of the SearchCriteria::LOGIC constants');
+		}
+
+		$this->logic = $logic;
 	}
 
 	/**
@@ -40,10 +57,12 @@ class SearchCriteria implements CriteriaInterface
 	 */
 	public function apply($query)
 	{
+		$method = $this->logic === static::LOGIC_AND ? 'where' : 'orWhere';
+
 		$query->where(function($query) {
 			$value = '%'.str_replace(' ', '%', $this->search).'%';
 			foreach ($this->columns as $column) {
-				$query->where($column, 'like', $value);
+				$query->$method($column, 'like', $value);
 			}
 		});
 	}
