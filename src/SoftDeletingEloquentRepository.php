3618b896fc23637b8dd2b08ea2b8415e411fbb32
Bradley Weston <brad@legalweb.org.uk>
2015-03-12 10:25:39 +0000

    Soft deleting Eloquent repository.

---
+++ src/SoftDeletingEloquentRepository.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Laravel 4 Repository classes
+ *
+ * @author   Andreas Lutro <anlutro@gmail.com>
+ * @license  http://opensource.org/licenses/MIT
+ * @package  l4-repository
+ */
+
+namespace anlutro\LaravelRepository;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SoftDeletingEloquentRepository extends EloquentRepository
+{
+
+    /**
+     * @var boolean
+     */
+    protected $onlyTrashed = false;
+
+    /**
+     * @var boolean
+     */
+    protected $withTrashed = false;
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Model $model
+     */
+    public function restore($model)
+    {
+        $model->restore();
+    }
+
+    /**
+     * Limit to trashed entities.
+     */
+    public function onlyTrashed()
+    {
+        $this->onlyTrashed = true;
+        $this->withTrashed = false;
+    }
+
+    /**
+     * Return trashed entites aswell.
+     */
+    public function withTrashed()
+    {
+        $this->onlyTrashed = false;
+        $this->withTrashed = true;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function newQuery()
+    {
+        if ($this->onlyTrashed === true) {
+            return call_user_func([$this->model, 'onlyTrashed']);
+        }
+
+        if ($this->withTrashed === true) {
+            return call_user_func([$this->model, 'withTrashed']);
+        }
+
+        return $this->model->newQuery();
+    }
+
+}
